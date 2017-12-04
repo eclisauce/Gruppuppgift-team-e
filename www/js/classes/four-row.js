a1c6b15ddf2b9d45a847fcf5519e32587661f345
Nana Mochihara <nanahaan@gmail.com>
2017-12-04 08:42:59 +0100

    add 'places' array as a class member

--- www/js/classes/four-row.js
+++ www/js/classes/four-row.js
@@ -2,24 +2,25 @@ class FourInALow {
   constructor() {
     this.render();
     this.turn = 'yellow';
+    this.places = [];
   }
 
   render(el) {
-    let places = new Array(7);
+    this.places = new Array(7);
     for (let i = 0; i < 7; i++) {
-      places[i] = new Array(6);
+      this.places[i] = new Array(6);
     }
 
     for (let i = 0, cx = 50; i < 7; i++) {
       for (let j = 0, cy = 50; j < 6; j++) {
-        places[i][j] = this.placeTemplate(cx, cy);
+        this.places[i][j] = this.placeTemplate(cx, cy);
         cy += 100;
       }
       cx += 100;
     }
     const board = `
     <svg class="bg-secondary" xmlns="http://www.w3.org/2000/svg" version="1.1">
-    ${places.join()}
+    ${this.places.join()}
     </svg>
     `
     $('#board').html(board);
