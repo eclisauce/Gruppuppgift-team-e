5d6d7ddad831b677d4ec7abcf482568a91b52023
Tin <tin.kotvas@gmail.com>
2017-12-06 08:49:24 +0100

    uppdaterat mall (package.json och app.js)

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// Require
 const express = require('express');
+const flexjson = require('jsonflex')();
+const compression = require('compression');
+
+// Create express server
 const app = express();
 
+// Express middleware
+app.use(compression());
+app.use(flexjson);
 app.use(express.static('www'));
 
-app.listen(3000, function () {
-  console.log('Webserver listening on port 3000');
-});
+// Start server
+app.listen(3000, () =>
+  console.log('Webserver listening on port 3000')
+);
