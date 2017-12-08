091ec2ad579d20a0bcfa202fcc115bb2003a3f91
Unknown <andreas@stickydirt.se>
2017-12-08 11:30:24 +0100

    Added d-none-classes on play-site

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,3 @@
-
-
 $(document).on('click', 'rect', function(){
   const targetCircle = $(this).siblings('circle');
   if (game.isClickable(targetCircle)) {
@@ -13,3 +11,14 @@ $(document).on('click', 'rect', function(){
 var height = $('footer').height() + 40;
 $('body').css({marginBottom: height});
 $(window).on('resize',function(){location.reload();});
+
+
+$('#mystartbutton').on('click', function(){
+  $('#myformhide').addClass('d-none').removeClass('d-block');
+  $('#mygamehide').addClass('d-block').removeClass('d-none');
+})
+
+$('#myquitbutton').on('click', function(){
+  $('#myformhide').addClass('d-block').removeClass('d-none');
+  $('#mygamehide').addClass('d-none').removeClass('d-block');
+})
