2f5b910df92073b99efb28180f980abd91b5dd53
Tin <tin.kotvas@gmail.com>
2017-12-13 13:34:51 +0100

    Added saveJSON, changed loadJSON and renderScore within Highscore.class.js. Changed path for jsonflex in index.html and layout because of renderScore changes.

--- www/js/classes/highscore.class.js
+++ www/js/classes/highscore.class.js
@@ -1,22 +1,44 @@
 class Highscore {
-    renderScore() {
-        $.getJSON('/highscore/highscore.json', function (data) {
-            let names = $("#hsNames");
-            let scores = $("#hsScores");
+    constructor() {
+        this.scores = [];
 
-            names.empty();
-            scores.empty();
-
-            names.append('<p class="m-0">Name</p>');
-            scores.append('<p class="m-0">Score</p>');
-
-            $.each(data.scores, function (index, value) {
-                names.append('<p class="m-0">' + this.name + '</p>');
-                scores.append('<p class="m-0">' + this.score + '</p>');
+        this.loadJSON();
+    }
+    loadJSON() {
+        let that = this;
+        $.getJSON('/json/highscore.json', function (data) {
+            that.scores = data.scores;
+        });
+    }
+    saveJSON() {
+        JSON._save('highscore', {
+            scores:this.scores.sort(function (a, b) {
+                return (a.score > b.score) ? 1 : ((b.score > a.score) ? -1 : 0);
             })
-        })
+        });
+    }
+    renderScore() {
+        let highscoreArea = $("#highscore");
+        let html = "";
+        highscoreArea.empty();
+        //create "columns"
+        highscoreArea.append('<div id="hsPosCol" class="col-2 p-0"><p class="text-right m-0">Position</p></div>')
+        highscoreArea.append('<div id="hsBlankCol" class="col-1 p-0"></div>')
+        highscoreArea.append('<div id="hsNameCol" class="col-6 p-0"><p class="m-0">Name</p></div>')
+        highscoreArea.append('<div id="hsScoreCol" class="col-3 p-0"><p class="m-0">Score</p></div>')
+        for (let i = 0; i < this.scores.length; i++) {
+            $("#hsPosCol").append(`<p class="text-right m-0">${i+1}</p>`);
+            $("#hsBlankCol").append('<p class="m-0">&nbsp;</p>');
+            $("#hsNameCol").append(`<p class="m-0">${this.scores[i].name}</p>`);
+            $("#hsScoreCol").append(`<p class="m-0">${this.scores[i].score}</p>`);
+        }
     }
 }
 
 const HS = new Highscore();
-HS.renderScore();
+
+
+function reloadAndRender(){
+    HS.loadJSON();
+    HS.renderScore();
+}
