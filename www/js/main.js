ab13514a7e3f1e330524c193fff6d7e0393c1835
Markus Andersson <make_99@hotmail.com>
2017-12-15 10:14:08 +0100

    Städar upp main.js lite

--- www/js/main.js
+++ www/js/main.js
@@ -1,14 +1,2 @@
 let game = new Game();
 $(window).resize(game.board.scale);
-
-$('#mystartbutton').on('click', function(){
-  $('#myformhide').addClass('d-none').removeClass('d-block');
-  $('#mygamehide').addClass('d-block').removeClass('d-none');
-  game.board.activate(true);
-})
-
-$('#myquitbutton').on('click', function(){
-  $('#myformhide').addClass('d-block').removeClass('d-none');
-  $('#mygamehide').addClass('d-none').removeClass('d-block');
-  game.board.activate(false);
-})
