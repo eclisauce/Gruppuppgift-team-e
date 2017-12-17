c0cfc26881af05aa0f2c12d88ad46476856b1eb6
Unknown <andreas@stickydirt.se>
2017-12-17 16:14:30 +0100

    Flyttat all rendering längst ner i game.class.js.

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -7,45 +7,6 @@ class Game {
     this.myButtons();
   }
 
-  renderInputForms() {
-    $('main').html(`
-      <h2>Spela 4 i Rad</h2>
-        <div class="row playerone pt-5">
-          <h4 class="col-12">Spelare 1:</h4>
-          <div class="form-group col-7 col-md-5 col-lg-4 pr-0">
-            <input type="text" maxlength="10" class="form-control" id="player1" placeholder="Ange spelare 1">
-          </div>
-
-          <div class="col-5 col-md-3 col-lg-2 pr-1">
-            <select id="p1type" class="custom-select bg-warning text-dark">
-              <option value="human" selected>Människa</option>
-              <option value="cp">Dator</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row playerone pt-2">
-          <h4 class="col-12">Spelare 2:</h4>
-          <div class="form-group col-7 col-md-5 col-lg-4 pr-0">
-            <input type="text" maxlength="10" class="form-control" id="player2" placeholder="Ange spelare 2">
-          </div>
-
-          <div class="col-5 col-md-3 col-lg-2 pr-1">
-            <select id="p2type" class="custom-select bg-danger text-white">
-              <option value="human" selected>Människa</option>
-              <option value="cp">Dator</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3 mb-5">
-          <div class="col-12">
-            <a id="startbutton" class="btn btn-success btn-lg" href="#l" role="button">Starta spel</a>
-          </div>
-        </div>
-      `);
-  }
-
   decidePicture(player){
     let picLink;
     if (player === 'human'){
@@ -57,37 +18,7 @@ class Game {
     return picLink
   }
 
-  renderBase() {
-    let player1Pic = this.decidePicture(this.player1.type);
-    let player2Pic = this.decidePicture(this.player2.type);
 
-    $('main').html(`
-      <div class="d-flex flex-column flex-md-row justify-content-around col-12 showplayersscale">
-        <div class="bg-warning text-white playerFont text-center mr-md-5 col-md-5 pt-1">
-          <h2 class="d-inline-block col-8 mt-4 pl-0">${this.player1.name}</h2>
-          <img class="img-fluid col-4 float-left p-0 pr-3 mb-0 pb-0" src="${player1Pic}">
-        </div>
-        <h1 class="mt-3 mt-lg-4 playerFont text-center"> VS </h1>
-        <div class="bg-danger text-white playerFont text-center ml-md-5 col-md-5 pt-1">
-          <h2 class="d-inline-block col-8 mt-4 pl-0">${this.player2.name}</h2>
-          <img class="img-fluid col-4 float-left p-0 pr-3 mb-0 pb-0" src="${player2Pic}">
-        </div>
-      </div>
-      <div class="row mt-3">
-        <div class="col-12">
-          <div id="board-holder">
-            <div id="board"></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row justify-content-center quitbuttonscale">
-        <div class="col-8 mt-4">
-          <button id="newgamebtn" class="btn btn-danger btn-lg btn-block">Avsluta spel</button>
-        </div>
-      </div>
-        `);
-  }
 
   randomName(){
     let nameArr = ["John Doe", "Sgt Nuke", "Skywalker", "Groot", "Batman", "John Doe", "The Dude", "James Bond", "Gandalf", "Terminator", "Yoda", "E.T", "Wolverine"];
@@ -179,5 +110,77 @@ class Game {
         hoverCircle.removeClass(("hoverYellow hoverRed"));
       }
     }, 'rect');
+  }
+
+  renderBase() {
+    let player1Pic = this.decidePicture(this.player1.type);
+    let player2Pic = this.decidePicture(this.player2.type);
+
+    $('main').html(`
+      <div class="d-flex flex-column flex-md-row justify-content-around col-12 showplayersscale">
+        <div class="bg-warning text-white playerFont text-center mr-md-5 col-md-5 pt-1">
+          <h2 class="d-inline-block col-8 mt-4 pl-0">${this.player1.name}</h2>
+          <img class="img-fluid col-4 float-left p-0 pr-3 mb-0 pb-0" src="${player1Pic}">
+        </div>
+        <h1 class="mt-3 mt-lg-4 playerFont text-center"> VS </h1>
+        <div class="bg-danger text-white playerFont text-center ml-md-5 col-md-5 pt-1">
+          <h2 class="d-inline-block col-8 mt-4 pl-0">${this.player2.name}</h2>
+          <img class="img-fluid col-4 float-left p-0 pr-3 mb-0 pb-0" src="${player2Pic}">
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-12">
+          <div id="board-holder">
+            <div id="board"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center quitbuttonscale">
+        <div class="col-8 mt-4">
+          <button id="newgamebtn" class="btn btn-danger btn-lg btn-block">Avsluta spel</button>
+        </div>
+      </div>
+        `);
+  }
+
+  renderInputForms() {
+    $('main').html(`
+      <h1>Spela 4 i Rad</h1>
+        <div class="row playerone pt-5">
+          <h4 class="col-12">Spelare 1:</h4>
+          <div class="form-group col-7 col-md-5 col-lg-4 pr-0">
+            <input type="text" maxlength="10" class="form-control" id="player1" placeholder="Ange spelare 1">
+          </div>
+
+          <div class="col-5 col-md-3 col-lg-2 pr-1">
+            <select id="p1type" class="custom-select bg-warning text-dark">
+              <option value="human" selected>Människa</option>
+              <option value="cp">Dator</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="row playerone pt-2">
+          <h4 class="col-12">Spelare 2:</h4>
+          <div class="form-group col-7 col-md-5 col-lg-4 pr-0">
+            <input type="text" maxlength="10" class="form-control" id="player2" placeholder="Ange spelare 2">
+          </div>
+
+          <div class="col-5 col-md-3 col-lg-2 pr-1">
+            <select id="p2type" class="custom-select bg-danger text-white">
+              <option value="human" selected>Människa</option>
+              <option value="cp">Dator</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="row mt-3 mb-5">
+          <div class="col-12">
+            <a id="startbutton" class="btn btn-success btn-lg" href="#l" role="button">Starta spel</a>
+          </div>
+        </div>
+      `
+    );
   }
 }
